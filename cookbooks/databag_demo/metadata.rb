fdd7e8567b9e67a659cfb4a45251a0e95ac12b6d
root <root@ip-172-31-8-221.us-west-2.compute.internal>
2017-08-17 06:59:13 -0400

    databag demo added

--- cookbooks/databag_demo/metadata.rb
+++ cookbooks/databag_demo/metadata.rb
@@ -1,7 +1,7 @@
-name             'apache'
+name             'databag_demo'
 maintainer       'YOUR_COMPANY_NAME'
 maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
-description      'Installs/Configures apache'
+description      'Installs/Configures databag_demo'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.0'
+version          '0.1.0'
